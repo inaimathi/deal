db241490d845d51de302bb9ac8147b36267bef95
inaimathi <inaimathi@lambda>
2013-07-15 17:35:00 -0400

    -define-handler now does proper lock-hoisting (when the body expression begins by taking a lock, the handler will look up the table and run table-related assertions, then acquire the table lock, then run the remaining assertions and actions together)

--- util.lisp
+++ util.lisp
@@ -64,7 +64,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; All for the custom define-handler
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun type-exp (arg type)
+(defun type-expression (arg type)
   "Given a symbol name and a type, returns the expression to read that type from a string"
   (match type
     (:string 
@@ -76,24 +76,24 @@
     ((or :keyword :facing)
      `(intern (string-upcase ,arg) :keyword))
     (:table 
-     (lookup-exp arg '(private-tables *server*) '(public-tables *server*)))
+     (lookup-expression arg '(private-tables *server*) '(public-tables *server*)))
     ((list :list _)
      `(loop for elem in (decode-json-from-string ,arg)
 	 collect (gethash elem (things table))))
     ((or :stack :flippable :placeable
 	 (list :card :from-table))
-     (lookup-exp arg '(things table)))
+     (lookup-expression arg '(things table)))
     ((list :card :from-hand)
-     (lookup-exp arg '(hand *player*)))
+     (lookup-expression arg '(hand *player*)))
     (_ (error "Invalid type label: '~a'" type))))
 
-(defun lookup-exp (arg &rest places)
+(defun lookup-expression (arg &rest places)
   (with-gensyms (sym)
     `(let ((,sym (intern (string-upcase ,arg) :keyword)))
        (or ,@(loop for p in places
 		collect `(gethash ,sym ,p))))))
 
-(defun lookup-assn (arg type)
+(defun lookup-assertion (arg type)
   (match type
     (:table `(assert (typep ,arg 'table)))
     (:stack `(assert (typep ,arg 'stack)))
@@ -111,25 +111,38 @@
 - The names (for use as final args)
 - The lookup assertions"
   (loop for (name type) in args
-     when (aif (type-exp name type) (list name it)) collect it into convs
-     collect name into as 
-     when (lookup-assn name type) collect it into assrs
-     finally (return (values convs as assrs))))
+     for table? = (eq type :table)
+     for t-exp = (type-expression name type)
+     for assn = (lookup-assertion name type)
+     collect name into arguments 
+     if table? collect (list name t-exp) into table-lookups and collect assn into table-assertions
+     when (and (not table?) t-exp) collect (list name t-exp) into type-expressions
+     when (and (not table?) assn) collect it into assertions
+     finally (return (values arguments table-lookups table-assertions type-expressions assertions))))
 
 (defmacro define-handler ((name) (&rest args) &body body)
   "Defines handlers with an eye for self-documentation, DRY and portability"
   (let* ((uri (concatenate 'string "/" (string-downcase (symbol-name name)))) 
 	 (opts `(,name :uri ,uri)))
-    (multiple-value-bind (type-conversion final-args lookup-assertions) (type-pieces args)
+    (multiple-value-bind (final-args table-lookups table-assertions type-conversions lookup-assertions) (type-pieces args)
       `(progn
-	 (setf (gethash ,uri *handlers*) (list ,@(mapcar (lambda (s) (string-downcase (symbol-name s))) final-args)))
+	 (setf (gethash ,uri *handlers*) '(,@args))
 	 ,(if (not args)
 	      `(define-easy-handler ,opts nil (encode-json-to-string (progn ,@body)))
-	      `(define-easy-handler ,opts ,final-args
-		 (assert (and ,@final-args))
-		 (let* ,type-conversion
-		   ,@lookup-assertions
-		   (encode-json-to-string ,@body))))))))
+	      (if (and (eq (caar body) 'with-lock-held) table-lookups)
+		  `(define-easy-handler ,opts ,final-args
+		     (assert (and ,@final-args))
+		     (let* ,table-lookups
+		       ,@table-assertions
+		       (with-lock-held ,(cadar body)
+			 (let* ,type-conversions
+			   ,@lookup-assertions
+			   (encode-json-to-string (progn ,@(cddar body)))))))
+		  `(define-easy-handler ,opts ,final-args
+		     (assert (and ,@final-args))
+		     (let* ,(append table-lookups type-conversions)
+		       ,@(append table-assertions lookup-assertions)
+		       (encode-json-to-string ,@body)))))))))
 
 (defmacro define-sse-handler ((name) (&rest args) &body body)
   `(define-handler (,name) (,@args)
