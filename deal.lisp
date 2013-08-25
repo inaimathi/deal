d14d3934d2202918a81fb42fa1f090bb6cdd8d24
inaimathi <inaimathi@lambda>
2013-08-25 13:39:32 -0400

    -define-handler has been split into three separate macros. There's a bit more code duplication now, but the underlying structure is now much simpler to understand. -added table assertions for seated players

--- deal.lisp
+++ deal.lisp
@@ -11,11 +11,9 @@
 		       (public-tables *server*)))
     (decks . ,(mapcar #'car (decks *server*)))))
 
-(define-handler (look-table) ((table :table))
-  (redact table))
+(define-handler (look-table) ((table :table)) (redact table))
 
-(define-handler (table-history) ((table :table))
-  (history table))
+(define-handler (table-history) ((table :table)) (history table))
 
 (define-handler (my-hand) ()
    (hash-values (hand (session-value :player))))
@@ -57,110 +55,97 @@
 	(publish! *server* :started-table `((id . ,id) (tag . ,tag) (seated . ,(length players)) (of . ,max-players))))
       (redact table))))
 
-(define-handler (lobby/join-table) ((table :table) (passphrase :string))
-  (with-table-lock
-      (with-slots (id tag players max-players) table
-	(let ((player (make-instance 'player))
-	      (len (length players)))
-	  (assert (>= max-players len))
-	  (setf (session-value :player) player)
-	  (insert! table player)
-	  (publish! table :joined)
-	  (publish! *server*
-		    (if (>= (+ 1 len) max-players) :filled-table :joined)
-		    `((id . ,id)))
-	  (redact table)))))
+(define-table-handler (lobby/join-table) ((table :table) (passphrase :string))
+  (with-slots (id tag players max-players) table
+    (let ((player (make-instance 'player))
+	  (len (length players)))
+      (assert (>= max-players len))
+      (setf (session-value :player) player)
+      (insert! table player)
+      (publish! table :joined)
+      (publish! *server*
+		(if (>= (+ 1 len) max-players) :filled-table :joined)
+		`((id . ,id)))
+      (redact table))))
 
 ;;;; Game related (once you're already at a table)
-(define-handler (play/speak) ((table :table) (message :string))
+(define-player-handler (play/speak) ((table :table) (message :string))
   (when (> (length message) 0)
     (publish! table :said `((message . ,(escape-string (take 255 message))))))
   :ok)
 
-(define-handler (play/move) ((table :table) (thing :placeable) (x :int) (y :int) (z :int) (rot :int))
-  (with-table-lock
-    (set-props thing x y z rot)
-    (publish! table :moved  `((thing . ,(id thing)) (x . ,x) (y . ,y) (z . ,z) (rot . ,rot)))
+(define-player-handler (play/move) ((table :table) (thing :placeable) (x :int) (y :int) (z :int) (rot :int))
+  (set-props thing x y z rot)
+  (publish! table :moved  `((thing . ,(id thing)) (x . ,x) (y . ,y) (z . ,z) (rot . ,rot)))
+  (redact table))
+
+(define-player-handler (play/take-control) ((table :table) (thing :placeable))
+  (setf (belongs-to thing) (id (session-value :player)))
+  (publish! table :took-control `((thing . ,(id thing))))
+  (redact table))
+
+(define-player-handler (play/flip) ((table :table) (thing :flippable))
+  (setf face (if (eq face :up) :down :up))
+  (publish! table :flipped `((thing . ,(redact thing))))
+  (redact table))
+
+(define-player-handler (play/new-stack-from-cards) ((table :table) (cards (:list card)))
+  (let* ((c ())first cards
+	 (stack (make-instance 'stack :belongs-to (session-value :player) :x (x c) :y (y c) :z (z c) :rot (rot c))))
+    (loop for card in cards 
+       do (remhash (id card) (things table))
+       do (insert! stack card))
+    (publish! table :stacked-up `((stack . ,(redact stack)) (cards . ,(mapcar #'id cards))))
     (redact table)))
 
-(define-handler (play/take-control) ((table :table) (thing :placeable))
-  (with-table-lock
-    (setf (belongs-to thing) (id (session-value :player)))
-    (publish! table :took-control `((thing . ,(id thing))))
+(define-player-handler (play/new-stack-from-deck) ((table :table) (deck-name :string) (face :facing) (x :int) (y :int) (z :int) (rot :int))
+  (let ((stack (deck->stack (session-value :player) (cdr (assoc deck-name (decks *server*) :test #'string=)) :face face)))
+    (set-props stack x y z rot)
+    (insert! table stack)
+    (publish! table :new-deck `((name . ,deck-name) (stack . ,(redact stack))))
     (redact table)))
-
-(define-handler (play/flip) ((table :table) (thing :flippable))
-  (with-table-lock
-    (with-slots (face) thing
-      (setf face (if (eq face :up) :down :up))
-      (publish! table :flipped `((thing . ,(redact thing))))
-      (redact table))))
-
-(define-handler (play/new-stack-from-cards) ((table :table) (cards (:list card)))
-  (with-table-lock
-    (let* ((c (first cards))
-	   (stack (make-instance 'stack :belongs-to (session-value :player) :x (x c) :y (y c) :z (z c) :rot (rot c))))
-      (loop for card in cards 
-	 do (remhash (id card) (things table))
-	 do (insert! stack card))
-      (publish! table :stacked-up `((stack . ,(redact stack)) (cards . ,(mapcar #'id cards))))
-      (redact table))))
-
-(define-handler (play/new-stack-from-deck) ((table :table) (deck-name :string) (face :facing) (x :int) (y :int) (z :int) (rot :int))
-  (with-table-lock
-    (let ((stack (deck->stack (session-value :player) (cdr (assoc deck-name (decks *server*) :test #'string=)) :face face)))
-      (set-props stack x y z rot)
-      (insert! table stack)
-      (publish! table :new-deck `((name . ,deck-name) (stack . ,(redact stack))))
-      (redact table))))
 
 ;;;;; Stacks
-(define-handler (stack/play) ((table :table) (stack :stack) (x :int) (y :int) (z :int) (rot :int))
-  (with-table-lock
-    (let ((card (pop! stack)))
-      (set-props card x y z rot)
-      (insert! table card)
-      (publish! table :played-from-stack `((stack . ,(id stack)) (card . ,(redact card))))
-      (redact table))))
-
-(define-handler (stack/add-to) ((table :table) (stack :stack) (card (:card :from-table)))
-  (with-table-lock
-    (move! card table stack)
-    (publish! table :added-to-stack `((stack . ,(id stack)) (card . ,(id card))))
+(define-player-handler (stack/play) ((table :table) (stack :stack) (x :int) (y :int) (z :int) (rot :int))
+  (let ((card (pop! stack)))
+    (set-props card x y z rot)
+    (insert! table card)
+    (publish! table :played-from-stack `((stack . ,(id stack)) (card . ,(redact card))))
     (redact table)))
 
-(define-handler (stack/merge) ((table :table) (stacks (:list stack)))
-  (with-table-lock
-    (let ((stack (first stacks)))
-      (loop for s in (rest stacks)
-	 do (dolist (c (cards s)) (insert! c (cards stack)))
-	 do (remhash (id s) (things table)))
-      (publish! table :merged-stacks `((stack . ,(redact stack)) (stacks ,@(mapcar #'id stacks))))
-      (redact table))))
+(define-player-handler (stack/add-to) ((table :table) (stack :stack) (card (:card :from-table)))
+  (move! card table stack)
+  (publish! table :added-to-stack `((stack . ,(id stack)) (card . ,(id card))))
+  (redact table))
+
+(define-player-handler (stack/merge) ((table :table) (stacks (:list stack)))
+  (let ((stack (first stacks)))
+    (loop for s in (rest stacks)
+       do (dolist (c (cards s)) (insert! c (cards stack)))
+       do (remhash (id s) (things table)))
+    (publish! table :merged-stacks `((stack . ,(redact stack)) (stacks ,@(mapcar #'id stacks))))
+    (redact table)))
 
 ;;;;; Hand
-(define-handler (hand/play) ((table :table) (card (:card :from-hand)) (face :facing) (x :int) (y :int) (z :int) (rot :int))
-  (with-table-lock
-    (setf (face card) face (x card) x (y card) y (z card) z (rot card) rot)
-    (move! card (session-value :player) table)
-    (publish! table :played-from-hand `((card . ,(redact card))))
-    (redact table)))
+(define-player-handler (hand/play) ((table :table) (card (:card :from-hand)) (face :facing) (x :int) (y :int) (z :int) (rot :int))
+  (setf (face card) face (x card) x (y card) y (z card) z (rot card) rot)
+  (move! card (session-value :player) table)
+  (publish! table :played-from-hand `((card . ,(redact card))))
+  (redact table))
 
-(define-handler (hand/play-to) ((table :table) (card (:card :from-hand)) (stack :stack))
-  (with-table-lock
-    (move! card (session-value :player) stack)
-    (publish! table :played-to-stack `((stack . ,(id stack)) (card . ,(redact card))))
-    (redact table)))
+(define-player-handler (hand/play-to) ((table :table) (card (:card :from-hand)) (stack :stack))
+  (move! card (session-value :player) stack)
+  (publish! table :played-to-stack `((stack . ,(id stack)) (card . ,(redact card))))
+  (redact table))
 
-(define-handler (hand/pick-up) ((table :table) (card (:card :from-table)))
-  (with-table-lock
-    (move! card table (session-value :player))
-    (publish! table :picked-up `((card . ,(id card))))
-    (redact table)))
+(define-player-handler (hand/pick-up) ((table :table) (card (:card :from-table)))
+  (move! card table (session-value :player))
+  (publish! table :picked-up `((card . ,(id card))))
+  (redact table))
 
 ;;;;; Non-table handlers
 ;;; These handlers don't respond with a redacted table object because it wouldn't make sense in context
-(define-handler (play/roll) ((table :table) (num-dice :int) (die-size :int) (modifier :int))
+(define-player-handler (play/roll) ((table :table) (num-dice :int) (die-size :int) (modifier :int))
   (assert (and (> num-dice 0) (> die-size 0)))
   (let ((mod (cond ((> modifier 0) (format nil "+~a" modifier))
 		   ((> 0 modifier) modifier)
@@ -171,25 +156,22 @@
 		  (total . ,(+ total modifier))
 		  (rolls . ,rolls))))))
 
-(define-handler (play/coin-toss) ((table :table))
+(define-player-handler (play/coin-toss) ((table :table))
   (publish! table :flipped-coin `((result . ,(pick (list :heads :tails))))))
 
-(define-handler (stack/draw) ((table :table) (stack :stack) (num :int))
-  (with-table-lock
-    (loop with rep = (min num (card-count stack)) repeat rep
-       do (insert! (session-value :player) (pop! stack)))
-    (publish! table :drew-from `((stack . ,(id stack)) (count . ,num)))
-    (hash-values (hand (session-value :player)))))
+(define-player-handler (stack/draw) ((table :table) (stack :stack) (num :int))
+  (loop with rep = (min num (card-count stack)) repeat rep
+     do (insert! (session-value :player) (pop! stack)))
+  (publish! table :drew-from `((stack . ,(id stack)) (count . ,num)))
+  (hash-values (hand (session-value :player))))
 
-(define-handler (stack/peek-cards) ((table :table) (stack :stack) (min :int) (max :int))
-  (with-table-lock
-      (publish! table :peeked `((stack . ,(id stack)) (count . ,(- max min))))
-    (take (- max min) (drop (+ min 1) (cards stack)))))
+(define-player-handler (stack/peek-cards) ((table :table) (stack :stack) (min :int) (max :int))
+  (publish! table :peeked `((stack . ,(id stack)) (count . ,(- max min))))
+  (take (- max min) (drop (+ min 1) (cards stack))))
 
-(define-handler (stack/show) ((table :table) (stack :stack) (min :int) (max :int))
-  (with-table-lock
-      (publish! table :revealed 
-		`((stack . ,(id stack)) (cards ,@(take (- max min) (drop (+ min 1) (cards stack))))))))
+(define-player-handler (stack/show) ((table :table) (stack :stack) (min :int) (max :int))
+  (publish! table :revealed 
+	    `((stack . ,(id stack)) (cards ,@(take (- max min) (drop (+ min 1) (cards stack)))))))
 
 ;; (define-handler (stack/reorder) ((table :table) (stack :stack) (min :int) (max :int))
 ;;   ;; TODO
--- define-handler.lisp
+++ define-handler.lisp
@@ -46,16 +46,16 @@
 
 (defun type-pieces (args)
   "Takes a list of arguments and returns five values:
-- The final argument list
-- The table lookups
-- The table-related assertions
-- The conversion expressions
-- The lookup assertions for all non-tables
+- The final argument list (symbols with no type annotations)
+- The table lookup expressions
+- The table-related assertion expressions
+- The other conversion expressions
+- The lookup assertion expressions for all non-table arguments
 
   Tables and others have to be separaed because the other lookups/assertions
-need access to a valid Table object to do anything other than error."
-  (loop for (name type) in args
-     for table? = (eq type :table)
+need access to a valid Table object to do anything other than error. Tables
+also need to be treated specially by some of the handler-definition macros."
+  (loop for (name type) in args for table? = (eq type :table)
      for t-exp = (type-expression name type)
      for assn = (lookup-assertion name type)
      collect name into arguments 
@@ -64,29 +64,52 @@ need access to a valid Table object to do anything other than error."
      when (and (not table?) assn) collect it into assertions
      finally (return (values arguments table-lookups table-assertions type-expressions assertions))))
 
+(defmacro with-handler-prelude (&body body)
+  `(progn
+     (assert (symbolp name) nil "`name` must be a symbol")
+     (let* ((uri (concatenate 'string "/" (string-downcase (symbol-name name))))
+	    (opts (list name :uri uri)))
+       (setf (gethash uri *handlers*) args)
+       (multiple-value-bind (final-args table-lookups table-assertions type-conversions lookup-assertions)
+	   (type-pieces args)
+	 ,@body))))
+
 (defmacro define-handler ((name) (&rest args) &body body)
-  "Defines handlers with an eye for self-documentation, DRY and portability"
-  (let* ((uri (concatenate 'string "/" (string-downcase (symbol-name name)))) 
-	 (opts `(,name :uri ,uri)))
-    (multiple-value-bind (final-args table-lookups table-assertions type-conversions lookup-assertions) (type-pieces args)
-      `(progn
-	 (setf (gethash ,uri *handlers*) '(,@args))
-	 ,(if (not args)
-	      `(define-easy-handler ,opts nil (encode-json-to-string (progn ,@body)))
-	      (if (and (listp (car body)) (eq (caar body) 'with-table-lock))
-		  (progn
-		    (assert table-lookups nil "You used with-table-lock, but no :table argument in `~a`" name)
-		    `(define-easy-handler ,opts ,final-args
-		       (assert (and ,@final-args))
-		       (let* ,table-lookups
-			 ,@table-assertions
-			 (with-lock-held ((lock ,(caar table-lookups)))
-			   (let* ,type-conversions
-			     ,@lookup-assertions
-			     (encode-json-to-string (progn ,@(cdar body)))))
-			 ,@(cdr body))))
-		  `(define-easy-handler ,opts ,final-args
-		     (assert (and ,@final-args))
-		     (let* ,(append table-lookups type-conversions)
-		       ,@(append table-assertions lookup-assertions)
-		       (encode-json-to-string (progn ,@body))))))))))
+  "Defines standard handlers (those with no need for complex table interactions, and those with no arguments)"
+  (with-handler-prelude
+    `(define-easy-handler ,opts ,final-args
+       ,@(if (not args)
+	     `((encode-json-to-string (progn ,@body)))
+	     `((assert (and ,@final-args))
+	       (let* ,(append table-lookups type-conversions)
+		 ,@(append table-assertions lookup-assertions)
+		 (encode-json-to-string (progn ,@body))))))))
+
+(defmacro define-table-handler ((name) (&rest args) &body body)
+  "Defines table-specific handlers. These need to establish a lock on the named table BEFORE doing the rest of their assertions/lookups."
+  (assert (eq :table (second (first args))) nil "First argument in a table handler must be a table.")
+  (with-handler-prelude
+    `(define-easy-handler ,opts ,final-args
+       (assert (and ,@final-args))
+       (let* ,table-lookups
+	 ,@table-assertions
+	 (with-lock-held ((lock ,(caar table-lookups)))
+	   (let* ,type-conversions
+	     ,@lookup-assertions
+	     (encode-json-to-string (progn ,@body))))))))
+
+(defmacro define-player-handler ((name) (&rest args) &body body)
+  "Defines a table-specific handler that checks whether the requester is a player at the named table.
+This could be folded in with define-table-handler, if not for lobby/join (which is a handler that both
+needs to establish a lock on the named table, AND must deal with players who are not sitting at the named table yet)."
+  (assert (eq :table (second (first args))) nil "First argument in a player handler must be a table.")
+  (with-handler-prelude
+    `(define-easy-handler ,opts ,final-args
+       (assert (and ,@final-args))
+       (let* ,table-lookups
+	 ,@table-assertions
+	 (assert (and (session-value :player) (member (session-value :player) (players ,(first (first args))))))
+	 (with-lock-held ((lock ,(caar table-lookups)))
+	   (let* ,type-conversions
+	     ,@lookup-assertions
+	     (encode-json-to-string (progn ,@body))))))))
